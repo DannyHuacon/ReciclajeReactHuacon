06c36d0d2150d97b06da0c450bfd06191e56da9b
DannyHuacon <55764753+DannyHuacon@users.noreply.github.com>
2023-02-08 18:24:39 -0500

    prueba

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,10 @@
+const ScoreBoard = ({ score }) => {
+  return (
+    <div className="score-board">
+       <h1>Proyecto Reciclaje React Danny Huacon</h1>
+      <h2>{score}</h2>
+    </div>
+  )
+}
+
+export default ScoreBoard
